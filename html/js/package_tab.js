fceedafc954e44b671515241e72d06e19c3b2bdc
jin <schoolyoung2m@naver.com>
2022-11-03 18:10:06 +0900

    22-11-03 / 메인페이지 반응형 완료

--- html/js/package_tab.js
+++ html/js/package_tab.js
@@ -1,5 +1,6 @@
 // 필요한 태그 생성
 const package_menu = document.querySelectorAll(".package_menu li");
+const text_color = document.querySelectorAll(".package_menu li a");
 const local = document.querySelectorAll(".package .right .local");
 
 //반복문을 이용하여 메뉴에 클릭기능 부여함
@@ -9,13 +10,15 @@ package_menu.forEach((item,index)=>{
         e.preventDefault();
         // 해당되지 않은 박스들은 안보이게
         local.forEach((item,index)=>{
-            // item.style.display = "none";
             item.style.opacity = 0;
             item.style.zIndex = 2;
+            package_menu[index].style.backgroundColor = "#dce8f9";
+            text_color[index].style.color = "#000";
+            
         });
-        // local[index].style.display = "flex";
         local[index].style.opacity = 1;
         local[index].style.zIndex = 3;
-
+        package_menu[index].style.backgroundColor = "#5d7cff";
+        text_color[index].style.color = "#fff";
     });
 });
